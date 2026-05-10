00b0cdd2c392b6f8ddaeb64b73269e8776c0acae
Marios Tsirogiannis <barkasgreco@gmail.com>
2026-05-10 15:41:50 +0200

    Add index.ts to export newsletter client and related types

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { createNewsletterClient } from "./client";
+export type { NewsletterClient } from "./client";
+export { ContentEdgeNewsletterError } from "./errors";
+export type {
+  EnquiryRequest,
+  NewsletterClientConfig,
+  PublicNewsletterResponse,
+  SubscribeRequest,
+} from "./types";
